8969b5fc49cbecfbd0211067e5c4624ee4472b3f
akobirrakhimov <@Akobiran>
2026-02-05 12:00:17 -0500

    Step 6 and 7 are added.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.List;
 public class Main{
     public static <T> void swap(Box<T> a, Box<T> b){
         T temp=a.getValue();
