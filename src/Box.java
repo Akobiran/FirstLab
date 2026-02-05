95b86c8d0ff4b3523e5402cf1c3aa49b067347b4
akobirrakhimov <@Akobiran>
2026-02-04 20:58:19 -0500

    Step 2 Generics.

---
+++ src/Box.java
@@ -0,0 +1,12 @@
+public class Box<T>{
+    private T value;
+    public Box (T value){
+        this.value=value;
+    }
+    public T getValue(){
+        return value;
+    }
+    public void setValue(T newValue){
+        this.value=newValue;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,20 @@
-public class Main{
+
+public class Main {
     public static void main(String[] args) {
-        Pokemon pikachu= new Pokemon("Pikachu ", "Electric");
-        Pokemon charmander= new Pokemon ("Charmander", "Fire");
+        Pokemon pikachu = new Pokemon("Pikachu ", "Electric");
+        Pokemon charmander = new Pokemon("Charmander", "Fire");
         System.out.println(pikachu);
         System.out.println(charmander);
-        PokeBox  box=new PokeBox(pikachu);
+        PokeBox box = new PokeBox(pikachu);
         System.out.println(box.get());
         box.set(charmander);
-        System.out.println(box.get());
+        System.out.println(box.get()+ "pokemon was changed !");
+        Box<Pokemon> pokenmonBox = new Box<>(pikachu);
+        Box<String> itemBox = new Box<>("Oran berry");
+        Box<Integer> levelBox = new Box<>(125);
+        System.out.println(pokenmonBox.getValue());
+        System.out.println(itemBox.getValue());
+        System.out.println(levelBox.getValue());
+
     }
 }
